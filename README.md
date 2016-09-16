6fb54846aac8478fc9aea263e948d0b449c69225
dvdmrn <damarino@cs.ubc.ca>
2016-09-16 12:39:33 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# phonologicalEditDistance
-Calculates the phonological edit distance within and between corpuses
+# Phonological Edit Distance
+This uses the mighty power of [Phonological Corpus Tools](http://phonologicalcorpustools.github.io/CorpusTools/) to calculate the average phonological edit distance of all items in a set. This can be used as a proxy measure of phonological dissimilarity.
+
+NOTE: there is more to come here, I'm just in the middle of writing a paper and am efficiently procrastinating by putting parts of this cool program up here.
+
+
+
+###dependencies:
+To run this you must have:
+    - [Python 3](https://www.python.org/download/releases/3.0/)  
+    - [Phonological Corpus Tools](http://phonologicalcorpustools.github.io/CorpusTools/)  
+    - [numpy](http://www.numpy.org/)  
+    
+
---
+++ phonoEditDistanceWITHINsubjects.py
@@ -0,0 +1,63 @@
+from corpustools.corpus import io
+from corpustools.symbolsim import phono_edit_distance
+import numpy
+import itertools
+
+#---------------------------------------------------------------------//
+#                                                                     //
+# calculates the phonological edit distance of all words within a set //
+#                                                                     //
+#---------------------------------------------------------------------//
+
+
+phonoSimList = []
+
+
+#Load a corpus. replace 'dummy.csv' with the corpus of your choice.
+#Ensure that the first 3 columns are headed by 'spelling','transcription', and 'frequency'.
+
+myCorpus = io.csv.load_corpus_csv(
+	"exampleCorpus", 
+	"dummy.csv",
+	",", 
+	".", 
+	annotation_types=None, 
+	feature_system_path=None, 
+	stop_check=None, 
+	call_back=None)
+
+
+print ("Loaded: ",myCorpus)
+
+#downloads a Hayes feature matrix
+io.binary.download_binary("ipa2hayes", "/matrix", call_back=None)
+
+ipa2hayes = io.binary.load_binary("/matrix")
+io.binary.save_binary(ipa2hayes, "/matrix")
+
+print("Features: ",ipa2hayes.features)
+
+#generate all unique combinations of 2 from the corpus
+wordCombinations = itertools.combinations(myCorpus.wordlist,2)
+
+#iterates through word combinations and calcuates the edit distance
+for wordCombo in wordCombinations:
+	print(wordCombo)
+	phonoEditDistance = phono_edit_distance.phono_edit_distance(
+				myCorpus.wordlist.get(wordCombo[0]),
+				myCorpus.wordlist.get(wordCombo[1]),
+				"transcription",
+				io.binary.load_binary("/matrix")
+				)
+	print("comparing: ",
+			myCorpus.wordlist.get(wordCombo[0]).transcription,
+			" to: ",
+			myCorpus.wordlist.get(wordCombo[1]).transcription, 
+			": ", 
+			phonoEditDistance
+			)
+	phonoSimList.append(phonoEditDistance)
+
+#the mean result of all edit distances from the corpus
+print("mean result: ", numpy.mean(phonoSimList))
+
